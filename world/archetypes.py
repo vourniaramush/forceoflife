c345e88779212f3b8c2b41a9d2dbbbddd0f179fa
Vourniaramush <FoL@user.noreply.github.com>
2016-01-12 02:23:32 -0600

    Added more information and fixed other information about the race and archetypes python files.

--- world/archetypes.py
+++ world/archetypes.py
@@ -1,13 +1,7 @@
 """
 Archetypes
 
-Human:
-Werewolves: subject to unlock
-Veela: subject to unlock
-Centaurs: subject to unlock
-Giants: subject to unlock
-goblins: subject to unlock
-house-elves: subject to unlock
+
 """
 
 from world.rulebook import roll_max
@@ -17,8 +11,7 @@ class ArchetypeException(Exception):
 	def __init__(self, msg):
 		self.msg = msg
 
-VALID_ARCHETYPES = ('Human', 'Werewolves', 'Veela', 'Centaurs', 
-					'Giants', 'Goblins', 'House-elves')
+VALID_ARCHETYPES = ()
 
 PRIMARY_TRAITS = ('Strength', 'Perception', 'Intelligence',
 				  'Dexterity', 'Charisma', 'Vitality', 'Magic')
--- world/races.py
+++ world/races.py
@@ -5,9 +5,35 @@ Its use is mainly during the character creation process.
 
 Selectable Races:
 
+Human:
+Werewolves: subject to unlock
+Veela: subject to unlock
+Centaurs: subject to unlock
+Giants: subject to unlock
+goblins: subject to unlock
+house-elves: subject to unlock
+
+sub-races of human/half-vampire/half-werewolf/half-giants
+
 pure-blood:
 half-blood:
 muggle-born:
 non-magical(muggle):
 squib: subject to unlock
 """
+
+from world.archetypes import Archetype
+
+class RaceException(Exception):
+	"""Base exception class for races module"""
+	def __init__(self, msg):
+		self.msg = msg
+
+ALL_RACES = ('Human', 
+		     'Werewolf', 'half-werewolf',
+		     'Veela', 'half-vampire',
+		     'Giants', 'half-giants',
+		     'goblins', 'half-goblin',
+		     'Centaurs', 'house-elves')
+
+ALL_SUB_RACES = ('pure-blood', 'half-blood', 'muggle-born', 'squib', 'non-magical')
